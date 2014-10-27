af43b7ffd5beaf62370bdf3aaad655cb72be3241
Bill Merrill <evilai@gmail.com>
2014-10-26 18:08:42 -0700

    parameterize the file

--- bam/test/client.py
+++ bam/test/client.py
@@ -1,13 +1,12 @@
 import requests
 
+model_file = '../bam/test-data/mto.tif'
 url = "http://127.0.0.1:8080/"
 s = requests.Session()
 kv = {'size': '200',
       'rez': '50'}      
       
-files = {'elevation': open('../bam/test-data/mto.tif', 'rb')}
-
-print files['elevation']
-
+files = {'elevation': open(model_file, 'rb')}
+print "Posting ", model_file
 r = s.post(url, params=kv, files=files)
 print r.text
