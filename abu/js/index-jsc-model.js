55cb8b750b362cab636e663549131e2f8444da08
Bill Merrill <evilai@gmail.com>
2014-10-30 09:59:23 -0700

    integrate jsc3d model viewer

---
+++ abu/js/index-jsc-model.js
@@ -0,0 +1,22 @@
+var indexModel = (function() {
+    var canvas,viewer;
+     
+    return {
+       init: function(displayCanvasId) {
+            canvas = document.getElementById(displayCanvasId);
+            viewer = new JSC3D.Viewer(canvas);
+       },
+       
+       showModel: function(modelUrl) {
+            viewer.setParameter('SceneUrl',         modelUrl);
+            viewer.setParameter('ModelColor',       '#CAA618');
+            viewer.setParameter('BackgroundColor1', '#E5D7BA');
+            viewer.setParameter('BackgroundColor2', '#383840');
+            viewer.setParameter('RenderMode',       'flat');
+            viewer.init();
+            viewer.update();
+       }
+       
+    } 
+    
+}());
