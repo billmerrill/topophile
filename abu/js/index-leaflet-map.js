0414a55a91dc15f55adfceaac5239417e5efbb33
Bill Merrill <evilai@gmail.com>
2014-10-30 11:32:18 -0700

    abu!

--- abu/js/index-leaflet-map.js
+++ abu/js/index-leaflet-map.js
@@ -4,7 +4,7 @@ var indexMap = (function() {
    
     return {
         init: function(mapDisplayId) {
-            map = L.map(mapDisplayId).setView([51.505, -0.09], 13);
+            map = L.map(mapDisplayId).setView([34.5, 131.6], 13);
             L.tileLayer('http://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
                 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
                 maxZoom: 18,
