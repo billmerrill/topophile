f59815bbe3af2956430fb46143abde1f61fc570d
Bill Merrill <evilai@gmail.com>
2014-11-05 10:29:10 -0800

    adjusts

--- abu/abu/js/index-jsc-model.js
+++ abu/abu/js/index-jsc-model.js
@@ -5,20 +5,30 @@ var indexModel = (function() {
        init: function(displayCanvasId) {
             canvas = document.getElementById(displayCanvasId);
             viewer = new JSC3D.Viewer(canvas);
+            viewer.setParameter('ModelColor',       '#9999FF');
+            viewer.setParameter('BackgroundColor1', '#DDDDDD');
+            viewer.setParameter('BackgroundColor2', '#DDDDDD');
+            viewer.setParameter('RenderMode',       'flat');
+            viewer.init();
+            viewer.update();
+       },
+       
+       ashowModel: function(modelUrl) {
+            viewer.setParameter('SceneUrl',         modelUrl);
+            // viewer.setParameter('ModelColor',       '#CAA618');
+            viewer.setParameter('ModelColor',       '#9999FF');
+            // viewer.setParameter('BackgroundColor1', '#E5D7BA');
+            // viewer.setParameter('BackgroundColor2', '#383840');
+            viewer.setParameter('BackgroundColor1', '#DDDDDD');
+            viewer.setParameter('BackgroundColor2', '#DDDDDD');
+            viewer.setParameter('RenderMode',       'flat');
+            // viewer.setParameter('Renderer',       'webgl');
+            viewer.init();
+            viewer.update();
        },
        
        showModel: function(modelUrl) {
-            viewer.setParameter('SceneUrl',         modelUrl);
-            // viewer.setParameter('ModelColor',       '#CAA618');
-            viewer.setParameter('ModelColor',       '#999999');
-            // viewer.setParameter('BackgroundColor1', '#E5D7BA');
-            // viewer.setParameter('BackgroundColor2', '#383840');
-            viewer.setParameter('BackgroundColor1', '#FFFFFF');
-            viewer.setParameter('BackgroundColor2', '#FFFFFF');
-            viewer.setParameter('RenderMode',       'flat');
-            viewer.setParameter('Renderer',       'webgl');
-            viewer.init();
-            viewer.update();
+           viewer.replaceSceneFromUrl(modelUrl);
        }
        
     } 
