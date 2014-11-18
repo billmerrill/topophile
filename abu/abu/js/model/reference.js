694c8970230c8a18bb0aa3ee4b1f19e6ecbfe729
Bill Merrill <evilai@gmail.com>
2014-11-18 11:41:36 -0800

    -us one dollar

--- abu/abu/js/model/reference.js
+++ abu/abu/js/model/reference.js
@@ -252,6 +252,48 @@ referenceObjects = (function() {
         
     },
 
+    USOneDollar = function(transform) {
+        var vertices = [
+            0.0, 0.0, 0.0,
+            0.0, 0.0, .11,
+            66.3, 0.0, 0.0,
+            66.3, 0.0, .11,
+            0.0, 156 , 0.0,
+            0.0, 156, .11,
+            66.3, 156, 0.0,
+            66.3, 156, .11 ];
+        
+        var indicies = [ 0, 2, 3, 1, -1,
+                         2, 6, 7, 3, -1,
+                         6, 4, 5, 7, -1,
+                         4, 0, 1, 5, -1,
+                         1, 3, 7, 5, -1,
+                         0, 4, 6, 2, -1];
+                         
+        if (transform) {
+            var vn = [];
+            var i = j = 0;
+            while (i < vertices.length) {
+                var nx = vertices[i] * transform['scale'][0] + transform['translate'][0]
+                var ny = vertices[i+1] * transform['scale'][1] + transform['translate'][1]
+                var nz = vertices[i+2] * transform['scale'][2] + transform['translate'][2]
+                i += 3;
+                vn[j++] = nx;
+                vn[j++] = ny;
+                vn[j++] = nz;
+            }
+            vertices = vn;
+        }
+        
+        var mesh = new JSC3D.Mesh;
+        mesh.name = 'token';
+        mesh.isDoubleSided = false;
+
+        mesh.vertexBuffer = vertices;
+        mesh.indexBuffer = indicies;
+        mesh.init();
+        return mesh;
+    }; 
 
     getUSQuarterChip = function(transform) {
         
@@ -300,9 +342,8 @@ referenceObjects = (function() {
   
     return {
         token: function(transform) {
-            // return getUSQuarterChip(transform);
-            // return getUSQuarter(transform);
-            return USQuarter(transform);
+            // return USQuarter(transform);
+            return USOneDollar(transform);
         },
         quarter: function() {
             
