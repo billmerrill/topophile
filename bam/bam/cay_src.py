4b00409628cf9b93a6656abb174c33c948efb8f4
Bill Merrill <evilai@gmail.com>
2014-12-03 12:10:46 -0800

    -remove cruft from geobrains src

--- bam/bam/cay_src.py
+++ bam/bam/cay_src.py
@@ -9,21 +9,10 @@ def make_img_filename(nwlat, nwlon, selat, selon):
     
 
 def get_elevation_url_parts(nwlat, nwlon, selat, selon):
-    # http://geobrain.laits.gmu.edu/cgi-bin/gbwcs-dem?service=wcs&version=1.0.0&request=getcoverage&coverage=SRTM_90m_Global&bbox=-90,38,-89,39&crs=epsg:4326&format=image/geotiff&store=true
     # http://climb.local/cgi-bin/mapserv?FORMAT=image/tiff&REQUEST=GetCoverage&map=/Library/WebServer/Documents/cay/wcs.map&SERVICE=WCS&VERSION=1.0.0&coverage=gmted&CRS=epsg:4236&BBOX=-121.9870000,46.6867333,-121.5270000,47.0084000&RESX=0.002083&RESY=0.002083
     
-
-    # bbox = "-121.9870000,46.6867333,-121.5270000,47.0084000"
+    server_base = "http://127.0.0.1/cgi-bin/mapserv?"
     bbox = "%s,%s,%s,%s"% (nwlon, selat, selon, nwlat) #-90,38,-89,39
-    # bbox = "-90,38,-89,39"
-    # params = {'service':'wcs',
-    #     'version':'1.0.0',
-    #     'request':'getcoverage',
-    #     'coverage':'SRTM_90m_Global',
-    #     'bbox': bbox,
-    #     'crs':'epsg:4326',
-    #     'format':'image/geotiff',
-    #     'store':'true'}
     params = {
         'map':      '/Library/WebServer/Documents/cay/wcs.map',
         'SERVICE':  'WCS',
@@ -36,9 +25,6 @@ def get_elevation_url_parts(nwlat, nwlon, selat, selon):
         'RESY':     '0.002083',
         'FORMAT':   'image/tiff'
     }
-    
-    server_base = "http://climb.local/cgi-bin/mapserv?"
-
     
     return server_base, params
         
@@ -65,14 +51,12 @@ def get_elevation(nwlat, nwlon, selat, selon, retry = 0):
 def get_elevation_image(filename, base_url, params):
      filename = os.path.join(os.getcwd(), 'app/elevation_cache', filename)
      ret = {'file':None, 'status':None}
-     content_size = 0
      written = 0
      import pprint
      with closing(requests.get(base_url, params=params, stream=True)) as response:
      # Do things with the response here.
          pprint.pprint(response)
          ret['status'] = response.status_code
-        #  content_size = int(response.headers['content-length'])
          if response.status_code == 200:
              with open(filename, "wb") as save:
                  for chunk in response.iter_content(chunk_size=1024): 
@@ -82,19 +66,13 @@ def get_elevation_image(filename, base_url, params):
                          written = written + len(chunk)
              ret['file'] = filename
      
-     print ("Content-size: %s vs Written %s " % (content_size, written))
-     
-     if content_size != written:
-         print "\n###############################"
-         print ("Bad GB Elevation Image: content-size: %s vs received-size %s " % (content_size, written))
-         print "###############################\n"
-        #  ret['file'] = None
-         
+     print ("Bytes Written %s " % (written))
             
      return ret
              
              
 def main():
+    #bbox = "-121.9870000,46.6867333,-121.5270000,47.0084000"
     print get_elevation('38.2', '-90','38','-89.8')
     
 main()
