456f12bafe77d949fcad6ae43daf09aae8c15c69
Bill Merrill <evilai@gmail.com>
2015-01-06 14:31:31 -0800

    -link and upload to shapeways

--- dana/dana/web.py
+++ dana/dana/web.py
@@ -1,3 +1,6 @@
+import base64
+import json
+import os
 import random
 import time
 import cherrypy
@@ -21,12 +24,13 @@ class ClientStore(object):
             
         return (id, self.clients[id])
         
-    def new_upoader(model=model)
+    def new_uploader(self, model):
         id = str(random.getrandbits(8))
         self.clients[id] =  Client(
             consumer_key="ea29837886ad7e769872dd86e02e0bba50fcf02d",
             consumer_secret="7627ff80f3668369c02bb286fe31956d5b9d9dff",
-            callback_url = "http://localhost:9090:/upload?id=%smodel=%s" % (id,model))
+            callback_url = "http://localhost:9090/upload?id=%s&model=%s" % (id,model))
+        return (id, self.clients[id])
             
     def _new_client(self):
         id = str(random.getrandbits(8))
@@ -45,8 +49,8 @@ class ShapewaysService(object):
         self.lookup = TemplateLookup(directories=['html'])
   
     def build_model_message(self, model):
-        m = {'filename': 'Your New Model %s' % int(time.time()),
-            'hasrightsToModel': 1,
+        m = {'fileName': 'Your New Model %s.stl' % int(time.time()),
+            'hasRightsToModel': 1,
             'acceptTermsAndConditions': 1,
             'isPrivate': 1,
             'isForSale': 1,
@@ -65,8 +69,11 @@ class ShapewaysService(object):
                 			}
                         }}}
                         
-        with open(model_file, 'rb' as f):
-            p['file'] = base64.b64encode(f.read())
+        model_file = os.path.join(MODEL_DIR, model)
+        with open(model_file, 'rb') as f:
+            m['file'] = base64.b64encode(f.read())
+            
+        return m
    
     
     def render(self, template, params = {}):
@@ -84,9 +91,17 @@ class ShapewaysService(object):
         params = {'auth_url' : client.connect()}
         return self.render("start.html", params)
         
-    def upload(self, id, model):
-        client = self.clients.get(id)
-        client.add_model(self.build_model_message(model))
+    #http://localhost:9090:/upload?id=107model=xn69kk97zuxg-xn69jb1jbn2h.stl?&oauth_token=e246c8a38b100a5e07ec91d7a4f7eb22b0a737be&oauth_verifier=ad4e54
+    @cherrypy.expose
+    def upload(self, id, model,**kwargs):
+        # shapeways throwin in a ?
+        if model[-1] == '?':
+            model = model[:-1]
+        id, client = self.clients.get(id)
+        client.verify(kwargs['oauth_token'], kwargs['oauth_verifier'])
+        response = client.add_model(self.build_model_message(model))
+        cherrypy.response.headers['Content-Type']= 'application/json'
+        return json.dumps(response)
         
         
             
