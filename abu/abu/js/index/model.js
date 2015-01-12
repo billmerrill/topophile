eb9072a0d10a02df07e0f8a1e983957d3c77f014
Bill Merrill <evilai@gmail.com>
2015-01-12 14:26:50 -0800

    -use a setting for this "FaceCulling"

--- abu/abu/js/index/model.js
+++ abu/abu/js/index/model.js
@@ -4,7 +4,11 @@ var indexModel = (function() {
     var canvas,viewer;
      
     return {
-       init: function(displayCanvasId) {
+        v: function() {
+            return viewer;
+        },
+        
+        init: function(displayCanvasId) {
     		//JSC3D.console.setup('console-area', '120px');
             canvas = document.getElementById(displayCanvasId);
             viewer = new JSC3D.Viewer(canvas);
@@ -13,24 +17,19 @@ var indexModel = (function() {
             viewer.setParameter('BackgroundColor1', '#DDDDDD');
             viewer.setParameter('BackgroundColor2', '#DDDDDD');
             viewer.setParameter('RenderMode',       'texture');
-            // viewer.setParameter('RenderMode',       'texturesmooth');
             viewer.setParameter('Renderer',         'webgl');
             viewer.setParameter('InitRotationX',     '-80');
             viewer.setParameter('InitRotationY' ,    '30');
-            
+            viewer.setParameter('FaceCulling' ,    'off');
+           
+        
             viewer.init();
             viewer.update();
-       },
+        },
        
-       showModel: function(modelUrl) {
+        showModel: function(modelUrl) {
            viewer.replaceSceneFromUrl(modelUrl);
-           viewer.onloadingcomplete = function() {
-               var makeDoubleSided = function(child) {
-                   child.isDoubleSided = true;
-               };
-               this.getScene().forEachChild(makeDoubleSided);
-           }
-       }
+        }
     } 
     
 }());
