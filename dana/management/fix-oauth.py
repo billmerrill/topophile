9f550fa23707573bc0b95d849b35a7069985fd7f
Bill Merrill <evilai@gmail.com>
2015-02-23 10:48:46 -0800

    - a starting place for a script to fix oauth issues

---
+++ dana/management/fix-oauth.py
@@ -0,0 +1,9 @@
+client = Client(
+    consumer_key="1694232fb1ee4d3441d0970934f994885409c9e2",
+    consumer_secret="06e28ec74812adf61bb582e0ec218aede5fe3420",
+    oauth_token = "a66163431a2215c48d2c4cb75fb348f19642df31",
+    oauth_secret = "c3b86a32247cdd5777ec2bc181447adbf9fe945b"
+)
+    
+    callback_url="http://localhost:3000/callback"
+)
