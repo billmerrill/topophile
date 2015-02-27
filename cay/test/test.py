6b98bd21a2e963e7e663beb2f1c616f7bbaa84a3
Bill Merrill <evilai@gmail.com>
2015-02-27 11:38:45 -0800

    -bleh

---
+++ cay/test/test.py
@@ -0,0 +1,44 @@
+import requests
+
+model_file = '../bam/test-data/mto.tif'
+url = "http://127.0.0.1:8080/"
+      
+def run_post():
+    s = requests.Session()
+    kv = {'size': '200',
+          'rez': '50'}      
+    files = {'elevation': open(model_file, 'rb')}
+    print "Posting ", model_file
+    r = s.post(url, params=kv, files=files)
+    print r.text
+
+
+def run_model_get():
+    kv = {'size': '200',
+          'rez': '50',
+          'nwlat':'46.9290278',
+          'nwlon':'-121.8229167',
+          'selat':'46.7762500',
+          'selon':'-121.6701389',
+          'zfactor':'2.2',
+          'price': 'e',
+	  'hollow': 1
+          }      
+    r = requests.get(url, params=kv)      
+    print r.headers
+    print r.status_code
+    print r.text
+    
+def run_price_check():
+    test_model_name = "c0xbw8e1b50n-c22p78x48p8n"
+    test_url = url + "price"
+    kv = {'model_id': test_model_name,
+          'mult': 2.0}
+    r = requests.get(test_url, params=kv)
+    print test_url
+    # print r.headers
+    # print r.status_code
+    print r.text
+    
+# run_model_get()
+run_price_check()
