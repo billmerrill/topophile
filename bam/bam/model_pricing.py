a3d3c2e72d2bd66595753512ee2ee6a67db384ae
Bill Merrill <evilai@gmail.com>
2015-04-20 14:35:00 -0700

    - it's sales time

--- bam/bam/model_pricing.py
+++ bam/bam/model_pricing.py
@@ -1,3 +1,5 @@
+import datetime
+
 def get_markup_by_size(model_size):
     markup_table = {50: 5.18, 100:10.36, 200:20.70}
     return markup_table.get(model_size, 25.88)
@@ -5,4 +7,15 @@ def get_markup_by_size(model_size):
 def get_model_service_markup():
     # going on sale, half off april 11
     # return 20.73
-    return 10.37
+    # return 10.37
+    return time_based_sale_price()
+    
+
+def time_based_sale_price():
+    rn = datetime.datetime.now();
+    no_dollar_deadline = datetime.datetime(2015, 4, 26, 14, 27, 30, 972067)
+    
+    if rn < no_dollar_deadline: 
+        return 0.0
+    else:
+        return 20.73
