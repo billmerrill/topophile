50d09d640c7e672337ab422580bfa3fdc9a3e700
Bill Merrill <evilai@gmail.com>
2015-11-06 14:00:58 -0800

    - return 3.50

--- hakone/fabfile.py
+++ hakone/fabfile.py
@@ -22,14 +22,14 @@ apps = {'dotcom': { 'root':'/home/billmerrill/webapps/topophile_com',
 def uptime():
   run('uptime')
 
-  
+
 def name_release_dir():
     return 'topophile-%s' % datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
-    
-@task 
+
+@task
 def deploy_gone_fishing():
-    put('/Users/bill/topo/gaua/gone_fishing/*', 
+    put('/Users/bill/topo/gaua/gone_fishing/*',
         '/home/billmerrill/webapps/topophile_gone_fishing')
 
 
@@ -37,23 +37,23 @@ def pre_deploy():
     exists = run('test -d %s; echo $?' % deploy_root) == '0'
     if not exists:
         run('mkdir -p %s' % deploy_root)
-        
+
         with cd(deploy_root):
             run('git clone git@github.com:billmerrill/topophile.git')
-    
-        
+
+
 def get_version(tag=None):
     with cd(topo_root):
         run('git checkout master')
         run('git pull')
         if tag:
-            run('git checkout tags/%s' % tag)   
+            run('git checkout tags/%s' % tag)
 
 
 def file_exists(name):
     return run('test -e %s; echo $?' % name, quiet=True) == '0'
 
-    
+
 def cycle_backup(app):
     '''
     make a copy of the running files before deploying an app
@@ -62,24 +62,24 @@ def cycle_backup(app):
     print 'app backup path %s ' % app_backup_path
     if file_exists(app_backup_path):
         run('rsync -a %s/ %s.old/' % (app_backup_path, app_backup_path))
-    
+
     # cp running app to backup
     run('rsync -a %s/ %s/' % (apps[app]['root'], app_backup_path))
-    
+
 
 def deploy_static_app(app):
     cycle_backup(app)
-  
+
     app_dir = apps[app]['root']
-    
+
     # sanity check before nuking
     if len(app_dir) > 20:
         with cd(app_dir):
             print 'XXX rm -rf start'
-            
+
     with cd(topo_root):
         run('cp -r %s/* %s/' % (apps[app]['git_files'], apps[app]['root']))
-    
+
 def restart_wsgi_app(app):
     cmd = apps[app]['restart']
     if len(cmd) > 20:
@@ -89,7 +89,7 @@ def restart_wsgi_app(app):
 def deploy_all(tag=None, static_only=False):
     '''
     deploy topophile.com, ebeko, and bam
-    
+
     1. checkout
     2. copy ebeko
     3. copy topophile.com
