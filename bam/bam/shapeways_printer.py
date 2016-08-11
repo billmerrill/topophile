9bfe2c2732019dae2438b136cec9f7a2cc23b09a
Bill Merrill <evilai@gmail.com>
2016-08-10 23:23:05 -0700

    -redo app auths

--- bam/bam/shapeways_printer.py
+++ bam/bam/shapeways_printer.py
@@ -5,26 +5,33 @@ MODEL = 'model'
 ALT = 'alt'
 
 keyring = {
-    PROD : { MODEL : {
-                'consumer_key':     "ea29837886ad7e769872dd86e02e0bba50fcf02d",
-                'consumer_secret':  "7627ff80f3668369c02bb286fe31956d5b9d9dff",
-                'oauth_secret':     "700ff4c3291942bca5e5fd7c6195e073bd867444",
-                'oauth_token':      "d016feacb49497d9daddb778698969d542fb04fb"},
-                
-            ALT : {
-                'consumer_key':     "fa206ebf6c8e642d3ed0e4ee7be72e1ba4266f37",
-                'consumer_secret':  "06db7be844acc56473ac6ca8998cc2e50656e019",
-                'oauth_secret':     "8564c4f28aa7c73d0b9a32133a98e257762d9df4",
-                'oauth_token':      "b2c4268daaeff446328a0197ddbf722d8719ce02"
-            }
+    # model = Topophile Modeler
+    # alt = Topophile Model Helper
+    PROD: {
+        MODEL: {
+            'consumer_key':     "80a769f7e26672fd6e1489c256edcf5cc6187259",
+            'consumer_secret':  "df85f5d585929f4ec48c9fb19a9b251ca79767f6",
+            'oauth_token':      "97cc59d7c01cf0cc7ec328bec6641c1af8c7f19b",
+            'oauth_secret':     "b867c8067b583c9aeb159d3fcf449b81d6a55227"},
+
+        ALT: {
+            'consumer_key':     "45af704eeb304045413c1c40838dba815e7dd000",
+            'consumer_secret':  "2ddb324f36d88372cccea673966d6619be3ab4ad",
+            'oauth_token':      "fa219dd901f47f78b7403b4008599fe708c3d9d8",
+            'oauth_secret':     "8b7ef6b94a034f34be0c2e900f69b60bffee8c3e"
+        }
     },
-    DEV : { MODEL : {
-                'consumer_key':     "1694232fb1ee4d3441d0970934f994885409c9e2",
-                'consumer_secret':  "06e28ec74812adf61bb582e0ec218aede5fe3420",
-                'oauth_token':      "a66163431a2215c48d2c4cb75fb348f19642df31",
-                'oauth_secret':     "c3b86a32247cdd5777ec2bc181447adbf9fe945b"
-    }}
+    DEV: {
+        MODEL: {
+            'consumer_key':     "eebd23897cf4eb1b47f6edadd186291a6acea654",
+            'consumer_secret':  "7c0883ed2c8105b710ed07cf0b76dd63e95b81bc",
+            'oauth_token':      "562ef407136345dd61dcd602de4a7d079319f425",
+            'oauth_secret':     "dd4e2d8aaf28a6c9d55b00ccdc0b47761902f8b0"
+        }
+    }
 }
+
+
 keyring[DEV][ALT] = keyring[DEV][MODEL]
 
 
@@ -33,7 +40,7 @@ def new_shapeways_client(env=DEV, key=MODEL):
 
 
 def price_model(model_data, env=DEV, key=ALT):
-    
+
     client = new_shapeways_client(env, key)
 
     # 6 - white strong and flexible
@@ -41,15 +48,15 @@ def price_model(model_data, env=DEV, key=ALT):
     # 100 - full color plastic
 
     p = {'materials': [6],
-       'volume': model_data['volume-mm3'] / 1000000000, 
-       'area': model_data['area-mm2'] / 1000000,
-       'xBoundMin': 0.0,
-       'xBoundMax': model_data['x-size-mm'] / 1000,
-       'yBoundMin': 0.0,
-       'yBoundMax': model_data['y-size-mm'] / 1000,
-       'zBoundMin': 0.0,
-       'zBoundMax':model_data['z-size-mm'] / 1000 }
-      
+         'volume': model_data['volume-mm3'] / 1000000000,
+         'area': model_data['area-mm2'] / 1000000,
+         'xBoundMin': 0.0,
+         'xBoundMax': model_data['x-size-mm'] / 1000,
+         'yBoundMin': 0.0,
+         'yBoundMax': model_data['y-size-mm'] / 1000,
+         'zBoundMin': 0.0,
+         'zBoundMax': model_data['z-size-mm'] / 1000}
+
     rez = {}
     try:
         r = client.get_price(p)
@@ -58,5 +65,5 @@ def price_model(model_data, env=DEV, key=ALT):
     except Exception as e:
         print "Pricing failure"
         print type(e)
-        
-    return rez 
+
+    return rez
