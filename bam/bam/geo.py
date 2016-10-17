38e0522cff41e393b1f6d97fb4c3aca2351ba2c6
Bill Merrill <evilai@gmail.com>
2016-10-17 15:20:50 -0700

    - track srs in a geo point and always give geohash computations 4326

--- bam/bam/geo.py
+++ bam/bam/geo.py
@@ -1,27 +1,48 @@
 import geohash
+from pyproj import Proj, transform
+
+EPSG_3857 = "epsg:3857"
+EPSG_4326 = "epsg:4326"
 
 
 class LatLon(object):
-    def __init__(self, lat, lon):
+    def __init__(self, lat, lon, srs):
         self.lat = lat
         self.lon = lon
+        self.srs = srs
+        self.reproj_lat = None
+        self.reproj_lon = None
+
+    def reproject(self, in_lat, in_lon, src_srs):
+        in_proj = Proj(init=src_srs)
+        out_proj = Proj(init='epsg:4326')
+        lon,lat = transform(in_proj,out_proj,in_lon,in_lat)
+        return (lon, lat)
 
     def get_geohash(self):
+        if self.srs != EPSG_4326:
+            if not self.reproj_lat:
+                nlon, nlat = self.reproject(self.lat, self.lon, self.srs)
+                self.reproj_lat = nlat
+                self.reproj_lon = nlon
+            return geohash.encode(self.reproj_lat, self.reproj_lon)
+
         return geohash.encode(float(self.lat), float(self.lon))
 
 
 class BoundingBox(object):
-    def __init__(self, north, west, south, east):
+    def __init__(self, north, west, south, east, srs=EPSG_4326):
         self.north = north
         self.west = west
         self.south = south
         self.east = east
+        self.srs = srs
 
     def get_nw_corner(self):
-        return LatLon(self.north, self.west)
+        return LatLon(self.north, self.west, self.srs)
 
     def get_se_corner(self):
-        return LatLon(self.south, self.east)
+        return LatLon(self.south, self.east, self.srs)
 
     def get_geohash(self):
         return "%s-%s" % (self.get_nw_corner().get_geohash(), self.get_se_corner().get_geohash())
