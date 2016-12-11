d15b4d2e97550019774cc17b9466559b22b34646
Bill Merrill <evilai@gmail.com>
2016-12-11 10:14:49 -0800

    - tiger lines work

---
+++ ijen/rawr.py
@@ -0,0 +1,5 @@
+import shapefile
+from pprint import pprint
+
+state_data = shapefile.Reader("cb_2015_us_state_500k")
+
---
+++ ijen/shapetest.py
@@ -0,0 +1,28 @@
+import sys
+from osgeo import gdal
+ds = gdal.OpenEx( "cb_2015_us_state_500k.shp", gdal.OF_VECTOR )
+if ds is None:
+    print "Open failed.\n"
+    sys.exit( 1 )
+lyr = ds.GetLayerByName( "point" )
+lyr.ResetReading()
+for feat in lyr:
+    feat_defn = lyr.GetLayerDefn()
+    for i in range(feat_defn.GetFieldCount()):
+        field_defn = feat_defn.GetFieldDefn(i)
+        # Tests below can be simplified with just :
+        # print feat.GetField(i)
+        if field_defn.GetType() == ogr.OFTInteger or field_defn.GetType() == ogr.OFTInteger64:
+            print "%d" % feat.GetFieldAsInteger64(i)
+        elif field_defn.GetType() == ogr.OFTReal:
+            print "%.3f" % feat.GetFieldAsDouble(i)
+        elif field_defn.GetType() == ogr.OFTString:
+            print "%s" % feat.GetFieldAsString(i)
+        else:
+            print "%s" % feat.GetFieldAsString(i)
+    geom = feat.GetGeometryRef()
+    if geom is not None and geom.GetGeometryType() == ogr.wkbPoint:
+        print "%.3f, %.3f" % ( geom.GetX(), geom.GetY() )
+    else:
+        print "no point geometry\n"
+ds = None
